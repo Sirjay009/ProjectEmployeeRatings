e63dbe872cc7353c641bbf7fed75cf3f548f2601
Chima Obutte <obuttechima@rocketmail.com>
2024-10-07 22:58:46 +0000

    Add validation checks for exactly 6 values in provided data, for values to range from 0 to 5 and check values provided are integers

--- run.py
+++ run.py
@@ -29,7 +29,32 @@ def get_scores_data():
     print("Example: 0,3,5\n")
 
     data_str = input("Enter your data here: ")
-    print(f"The data provided is {data_str}")
+    #print(f"The data provided is {data_str}")
+
+    scores_data = data_str.split(",")
+    validate_data(scores_data)
+
+def validate_data(values):
+    """
+    Use 'try/except and for loop statements to check if data is valid.
+    Raise ValueError if strings cannot be converted into
+    integers, if there are not exactly 3 values or if out of 0 and 5 range.
+    """
+    #print(values)
+    try:
+        if len(values) != 3:
+            raise ValueError(
+                f"Exactly 3 values required, you provided {len(values)}"
+            )
+
+        for value in values:
+            int_value = int(value)
+            if not(0 <= int_value <= 5):
+                raise ValueError(f"Value {int_value} is out of range (0-5)")
+
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
+
 
 
 get_scores_data()
