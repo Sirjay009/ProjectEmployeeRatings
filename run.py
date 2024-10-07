e93902d7cfdb55b0e52cee638d73b9137bc6a56a
Chima Obutte <obuttechima@rocketmail.com>
2024-10-07 23:08:24 +0000

    Add user request loop to repeat request for data until data provided is valid

--- run.py
+++ run.py
@@ -12,9 +12,10 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("employee_ratings")
 
-#scores = SHEET.worksheet("employee_survey_data")
-#data = scores.get_all_values()
-#print(data)
+# scores = SHEET.worksheet("employee_survey_data")
+# data = scores.get_all_values()
+# print(data)
+
 
 def get_scores_data():
     """
@@ -23,16 +24,23 @@ def get_scores_data():
     the user via the terminal. The loop will repeatedly
     request data until it is valid
     """
-    print("Please enter your score ratings")
-    print("Data should be three numbers seperated by commas")
-    print("Data value should be between the range of 0-5")
-    print("Example: 0,3,5\n")
+    while True:
+        print("Please enter your score ratings")
+        print("Data should be three numbers seperated by commas")
+        print("Data value should be between the range of 0-5")
+        print("Example: 0,3,5\n")
 
-    data_str = input("Enter your data here: ")
-    #print(f"The data provided is {data_str}")
+        data_str = input("Enter your data here: ")
+        # print(f"The data provided is {data_str}")
 
-    scores_data = data_str.split(",")
-    validate_data(scores_data)
+        scores_data = data_str.split(",")
+
+        if validate_data(scores_data):
+            print("Data is valid")
+            break
+
+    return scores_data
+
 
 def validate_data(values):
     """
@@ -40,7 +48,7 @@ def validate_data(values):
     Raise ValueError if strings cannot be converted into
     integers, if there are not exactly 3 values or if out of 0 and 5 range.
     """
-    #print(values)
+    # print(values)
     try:
         if len(values) != 3:
             raise ValueError(
@@ -49,12 +57,14 @@ def validate_data(values):
 
         for value in values:
             int_value = int(value)
-            if not(0 <= int_value <= 5):
+            if not (0 <= int_value <= 5):
                 raise ValueError(f"Value {int_value} is out of range (0-5)")
 
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
+        return False
+
+    return True
 
 
-
-get_scores_data()
+data = get_scores_data()
